eddb63b37eac8ef3f60b1fade479fb80ed3431b9
Francisco Panis Kaseker <franciscopk.ilax@gmail.com>
2022-12-09 00:21:26 -0300

    adjust the data structure

--- scripts/plesk_get_vhosts_logs_files.py
+++ scripts/plesk_get_vhosts_logs_files.py
@@ -6,11 +6,10 @@ vhosts_dir="/var/www/vhosts/system/"
 vhosts=os.listdir(vhosts_dir)
 aux_prefix=vhosts_dir
 aux_suffix=["/logs/proxy_access_ssl_log","/logs/proxy_access_log","/logs/access_log ","/logs/access_ssl_log"]
-data={ "data" : []}
+data = []
 
 aux_count=0
 for vhost in vhosts:
 	for suffix in aux_suffix:
-		data["data"].append({"domain" : vhost, "log" : aux_prefix+vhost+suffix})
-
-print(json.dumps(data))
+		data.append({"{#HTTPDOMAIN}" : vhost, "{#HTTPLOGFILE}" : aux_prefix+vhost+suffix})
+print(data)
