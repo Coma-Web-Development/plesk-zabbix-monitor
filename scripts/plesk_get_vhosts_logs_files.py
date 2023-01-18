ebebb2e2875be52434d4679b5fb473a5a288f0c3
Francisco Panis Kaseker <franciscopk.ilax@gmail.com>
2023-01-18 14:41:28 -0300

    close the file, remove the new line char

--- scripts/plesk_get_vhosts_logs_files.py
+++ scripts/plesk_get_vhosts_logs_files.py
@@ -6,10 +6,11 @@ hosts_file="/etc/plesk-zabbix-monitor"
 vhosts_dir="/var/www/vhosts/system/"
 
 if os.path.exists(hosts_file):
-        file = open(hosts_file, 'r')
-        vhosts = file.readlines()
+	file = open(hosts_file, 'r')
+	vhosts = [line.rstrip() for line in file.readlines()]
+	file.close()
 else:
-        vhosts=os.listdir(vhosts_dir)
+    vhosts=os.listdir(vhosts_dir)
 
 aux_prefix=vhosts_dir
 aux_suffix=["/logs/proxy_access_ssl_log","/logs/proxy_access_log","/logs/access_log ","/logs/access_ssl_log"]
@@ -18,7 +19,7 @@ data = []
 
 for vhost in vhosts:
     for suffix in aux_suffix:
-        file_absolut_path=''.join([aux_prefix,vhost.strip('\n'),suffix])
+        file_absolut_path=''.join([aux_prefix,vhost,suffix])
         if os.path.exists(file_absolut_path):
             data.append({"{#HTTPDOMAIN}" : vhost, "{#HTTPLOGFILE}" : file_absolut_path})
 
